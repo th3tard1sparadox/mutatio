b02deb5a108bc1fa03eef6a4f3ea3498ea10d432
Gustav Sörnäs <gustav@sornas.net>
2022-01-28 21:03:07 +0100

    state table and refactor out colors

--- main.lua
+++ main.lua
@@ -1,18 +1,27 @@
+LIGHT_BG = {0.93, 0.75, 0.92, 1}
+DARK_BG = {0.03, 0.08, 0.16, 1}
+
+state = {
+    swap_time = 2.0,
+    dark = false,
+}
+
 function love.load()
     success = love.window.setMode(800, 800)
-    love.graphics.clear(0.93, 0.75, 0.92, 1)
-    time = 0
-    dark = false
+end
+
+function love.update()
+    state.swap_time = state.swap_time - love.timer.getDelta()
+    if state.swap_time < 0 then
+        state.swap_time = 2.0
+        dark = not dark
+    end
 end
 
 function love.draw()
-    if love.timer.getTime() - time >= 2 then
-        time = love.timer.getTime()
-        dark = not dark
-    end
     if dark then
-        love.graphics.clear(0.03, 0.08, 0.16, 1)
+        love.graphics.clear(DARK_BG)
     else
-        love.graphics.clear(0.93, 0.75, 0.92, 1)
+        love.graphics.clear(LIGHT_BG)
     end
 end

5fdd6a9a611339b80920a4e2ba35e110ca6a3a22
Annie Wång <annie.u.wang@gmail.com>
2022-01-28 20:53:02 +0100

    switching time

--- main.lua
+++ main.lua
@@ -1,3 +1,18 @@
+function love.load()
+    success = love.window.setMode(800, 800)
+    love.graphics.clear(0.93, 0.75, 0.92, 1)
+    time = 0
+    dark = false
+end
+
 function love.draw()
-    love.graphics.print("hello world", 400, 300)
+    if love.timer.getTime() - time >= 2 then
+        time = love.timer.getTime()
+        dark = not dark
+    end
+    if dark then
+        love.graphics.clear(0.03, 0.08, 0.16, 1)
+    else
+        love.graphics.clear(0.93, 0.75, 0.92, 1)
+    end
 end
